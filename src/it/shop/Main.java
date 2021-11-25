4f2dc33a8dbde50eaaec81b58292637fcdb6f580
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-25 17:06:26 +0100

    Setter test

--- src/it/shop/Main.java
+++ src/it/shop/Main.java
@@ -4,14 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Prodotto prodotto = new Prodotto("Paperella di gomma", "Una STRABILIANTE paperella di gomma gialla", 17, 22);
+		Prodotto prodotto = new Prodotto("Paperella di gomma", "Una normalissima paperella di gomma gialla", 5, 22);
 		
 		// getter test
-		System.out.println("[get] codice prodotto: " + prodotto.getCodice());
-		System.out.println("[get] nome prodotto: " + prodotto.getNome());
-		System.out.println("[get] descrizione prodotto: " + prodotto.getDescrizione());
-		System.out.println("[get] prezzo prodotto: " + prodotto.getPrezzo() + "€");
-		System.out.println("[get] iva prodotto: " + prodotto.getIva() + "%");
+		System.out.println("codice prodotto: " + prodotto.getCodice());
+		System.out.println("nome prodotto: " + prodotto.getNome());
+		System.out.println("descrizione prodotto: " + prodotto.getDescrizione());
+		System.out.println("prezzo prodotto: " + prodotto.getPrezzo() + "€");
+		System.out.println("iva prodotto: " + prodotto.getIva() + "%");
+		System.out.println("prezzo con iva: " + prodotto.prezzoIva() + "€");
+
+		System.out.println();
+		
+		//setter test
+		prodotto.setNome("Paperella STRABILIANTE");
+		System.out.println("nome prodotto aggiornato: " + prodotto.getNome());
+		prodotto.setDescrizione("Una STRABILIANTE paperella di gomma gialla");
+		System.out.println("descrizione prodotto aggiornata: " + prodotto.getDescrizione());
+		prodotto.setPrezzo(99);
+		System.out.println("prezzo prodotto aggiornato: " + prodotto.getPrezzo() + "€");
+		prodotto.setIva(50);
+		System.out.println("iva prodotto aggiornata: " + prodotto.getIva() + "%");
+		
+		System.out.println("prezzo con iva: " + prodotto.prezzoIva() + "€");
 		
 	}
 
